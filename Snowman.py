98ff9ffeb301ae3b40f0189a84d7d89b68cfdcbf
lbonnemaison <luc.bonnemaison@datakhi.com>
2022-10-21 09:25:12 +0200

    add: New script

---
+++ Snowman.py
@@ -0,0 +1,11 @@
+
+sentence = input("What the sentence ?")
+
+def snowman(a):
+    try:
+        if a == "What say two snowman when they meet ?":
+            print("It smells carrots !!")
+    except:
+        print("You'll know it later ;)")
+
+snowman(sentence)
